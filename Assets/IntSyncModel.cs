d038e649cd55c7853d38246051dfde317b9d8b92
baxter <bblei0333@student.pps.net>
2024-11-05 11:40:28 -0800

    cry

--- Assets/IntSyncModel.cs
+++ Assets/IntSyncModel.cs
@@ -7,8 +7,5 @@ using Normal.Realtime.Serialization;
 [RealtimeModel]
 public partial class IntSyncModel {
 [RealtimeProperty(1, true, true)] 
-public static int _testnum;
-public static void increasenum(){
-    _testnum++;
-}
+public int _testnum;
 }
