5b88abea3b06d0ce346c60aa0d33f68138ed1544
Ryder Corbett <rcorbett6217@student.pps.net>
2025-02-07 10:32:05 -0800

    boom shit make raycast for real mouse cus unity is dumb

--- Assets/exploder.cs
+++ Assets/exploder.cs
@@ -7,9 +7,17 @@ public class ExampleClass : MonoBehaviour
     public float radius = 5.0F;
     public float power = 10.0F;
 
-    IEnumerator Shit(){
-        yield return new WaitForSeconds(2f);
-        Vector3 explosionPos = transform.position;
+    public bool boom = true;
+
+    
+
+    void Update(){
+        //Debug.Log(Input.mousePosition);
+        /*
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 explosionPos = mousePos;
+        if (Input.GetKeyDown(KeyCode.F)){
+            
         Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
         foreach (Collider hit in colliders)
         {
@@ -18,12 +26,37 @@ public class ExampleClass : MonoBehaviour
             if (rb != null)
                 rb.AddExplosionForce(power, explosionPos, radius, 1.0F);
         }
-    }
+             
+        }
+        */
+        if(boom){
+            boom = false;
+            StartCoroutine(Shit());
 
+        }
+    }
     void Start()
     {
         StartCoroutine(Shit());
 
+        
+    }
+    IEnumerator Shit(){
+        yield return new WaitForSeconds(2f);
+        boom = true;
+        Vector3 explosionPos = Input.mousePosition;
+        explosionPos.z = 1;
+        Debug.Log(explosionPos);
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(power, explosionPos, radius, 0.001F);
+                Debug.Log("BOOM");
+        }
+        
         
     }
 }
