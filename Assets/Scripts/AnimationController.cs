4ca1be674aabad07e98276966fa954b4e596d296
baxter <bblei0333@student.pps.net>
2025-04-28 11:41:07 -0700

    maybe stuff or suicide

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Normal.Realtime;
 using UnityEngine;
-using Normal.Realtime;
-using Normal.Realtime.Serialization;
 
 public class AnimationController : MonoBehaviour
 {
@@ -12,12 +10,17 @@ public class AnimationController : MonoBehaviour
     public bool flipped;
     private Renderer pieceRenderer;
     private Vector3 Posi;
-    // Start is called before the first frame update
     
-Realtime.InstantiateOptions option = new Realtime.InstantiateOptions {
+    // References to complete prefabs
+    public GameObject blackFlipPrefab;  // Bc with AnimatedBlack as child
+    public GameObject whiteFlipPrefab;  // Wc with AnimatedWhite as child
+    public GameObject shareFlipPrefab;  // Sc with AnimatedShare as child
+    
+    Realtime.InstantiateOptions option = new Realtime.InstantiateOptions {
         ownedByClient = true,
         preventOwnershipTakeover = true
     };
+    
     public void Grabbed(){
         palm = Instantiate(h1, Vector3.zero, Quaternion.identity);
         Animator animator3 = palm.GetComponent<Animator>();
@@ -35,43 +38,82 @@ Realtime.InstantiateOptions option = new Realtime.InstantiateOptions {
         GameObject.Find("GomokuBoard").GetComponent<GomokuControl>().AnimationOver(2);
         Destroy(fist);
     }
+    
     public void DoAFlip(int p1, int p2, Vector3 pos)
     {
-        if(flipped){return;};
+        if(flipped) return;
         flipped = true;
-        string b1, b2;
-        b1 = null;
-        b2 = null;
-        if(p1 == 1){b1 = "AnimatedWhite";};
-        if(p1 == 2){b1 = "AnimatedBlack";};
-        if(p1 == 3){b1 = "AnimatedShare";};
-        if(p2 == 1){b2 = "AnimatedWhite";};
-        if(p2 == 2){b2 = "AnimatedBlack";};
-        if(p2 == 3){b2 = "AnimatedShare";};
-        str2 = b2;
-        Debug.Log(pos);
-        flip1 = Realtime.Instantiate(b1,pos, Quaternion.identity, option);
-        Animation animator1 = flip1.GetComponent<Animation>();
-        animator1.Play("FlipUp");
-        StartCoroutine(Waiter());
+        
+        GameObject prefab1 = null;
+        GameObject prefab2 = null;
+        
+        // Determine which prefab to use based on piece type
+        if(p1 == 1) prefab1 = whiteFlipPrefab;
+        if(p1 == 2) prefab1 = blackFlipPrefab;
+        if(p1 == 3) prefab1 = shareFlipPrefab;
+        
+        if(p2 == 1) prefab2 = whiteFlipPrefab;
+        if(p2 == 2) prefab2 = blackFlipPrefab;
+        if(p2 == 3) prefab2 = shareFlipPrefab;
+        
+        if (prefab1 == null || prefab2 == null) {
+            Debug.LogError("Flip prefab is null! Check your piece types: " + p1 + ", " + p2);
+            return;
+        }
+        
+        // Store the name of the second prefab for later use
+        str2 = prefab2.name;
+        
+        // Instantiate the first prefab
+        flip1 = Realtime.Instantiate(prefab1.name, pos, Quaternion.identity, option);
+        
+        // Use the AnimationStateSync component to play the animation on all clients
+        AnimationStateSync animSync = flip1.GetComponent<AnimationStateSync>();
+        if (animSync != null)
+        {
+            animSync.PlayFlipUp();
+        }
+        else
+        {
+            Debug.LogWarning("AnimationStateSync component not found on " + prefab1.name);
+        }
+        
+        StartCoroutine(Waiter(pos));
     }
-    IEnumerator Waiter(){
-        yield return new WaitForSeconds(100f);
-        flip2 = Realtime.Instantiate(str2,flip1.transform.position, flip1.transform.rotation, option);
+    
+    IEnumerator Waiter(Vector3 pos)
+    {
+        yield return new WaitForSeconds(1.0f);
+        
+        // Instantiate the second prefab
+        flip2 = Realtime.Instantiate(str2, pos, Quaternion.identity, option);
+        
+        // Use the AnimationStateSync component to play the animation on all clients
+        AnimationStateSync animSync = flip2.GetComponent<AnimationStateSync>();
+        if (animSync != null)
+        {
+            animSync.PlayFlipDown();
+        }
+        else
+        {
+            Debug.LogWarning("AnimationStateSync component not found on " + str2);
+        }
+        
         Realtime.Destroy(flip1);
-        Animation animator2 = flip2.GetComponent<Animation>();
-        animator2.Play("FlipDown");
+        
         yield return new WaitForSeconds(0.8f);
         GameObject.Find("GomokuBoard").GetComponent<GomokuControl>().AnimationOver(1);
         Realtime.Destroy(flip2);
+        flipped = false;
     }
-    //public void OnAnimationEnd(){
-    //}
 
     void Start(){
+        // Validate prefab references
+        if (blackFlipPrefab == null || whiteFlipPrefab == null || shareFlipPrefab == null) {
+            Debug.LogError("One or more flip prefabs not assigned in the inspector. Please assign all prefabs.");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
---
+++ Assets/Scripts/AnimationStateSync.cs
@@ -0,0 +1,61 @@
+using Normal.Realtime;
+using UnityEngine;
+
+public class AnimationStateSync : RealtimeComponent<AnimationStateModel>
+{
+    private Animation _animation;
+    
+    private void Awake()
+    {
+        _animation = GetComponentInChildren<Animation>();
+    }
+    
+    protected override void OnRealtimeModelReplaced(AnimationStateModel previousModel, AnimationStateModel currentModel)
+    {
+        if (previousModel != null)
+        {
+            previousModel.animationStateDidChange -= OnAnimationStateChanged;
+        }
+        
+        if (currentModel != null)
+        {
+            currentModel.animationStateDidChange += OnAnimationStateChanged;
+            
+            // Apply the current state immediately
+            UpdateAnimationState(currentModel.animationState);
+        }
+    }
+    
+    private void OnAnimationStateChanged(AnimationStateModel model, int value)
+    {
+        UpdateAnimationState(value);
+    }
+    
+    private void UpdateAnimationState(int state)
+    {
+        if (_animation == null) return;
+        
+        switch (state)
+        {
+            case 1:
+                _animation.Play("FlipUp");
+                break;
+            case 2:
+                _animation.Play("FlipDown");
+                break;
+            default:
+                // No animation or stop current
+                break;
+        }
+    }
+    
+    public void PlayFlipUp()
+    {
+        model.animationState = 1;
+    }
+    
+    public void PlayFlipDown()
+    {
+        model.animationState = 2;
+    }
+}
